7494be85c1e379062e9ad4cc01b4484eb20d135d
Den Hafiz <den.m.hafiz@gmail.com>
2026-08-31 23:24:14 +0800

    Fail test to check if CI would fail

---
+++ internal/auth/get_api_key_test.go
@@ -0,0 +1,34 @@
+package auth
+
+import (
+	"errors"
+	"net/http"
+	"testing"
+)
+
+func TestGetAPIKey(t *testing.T) {
+
+	h := http.Header{}
+
+	api, err := GetAPIKey(h)
+	if api != "" && err != errors.New("no authorization header included") {
+		t.Error("Empty auth produce API and/or not provide the correct error.")
+	}
+
+	// Test for <2 value of auth
+	h.Add("Authorization", "Test")
+	api, err = GetAPIKey(h)
+	if api != "" && err != errors.New("malformed authorization header") {
+		t.Error("Malform data produce non-empty API key or wrong error")
+	}
+
+	// Test for first value != ApiKey
+	h.Add("Authorization", "NotApiKey TEst")
+	api, err = GetAPIKey(h)
+	if api != "" && err != errors.New("malformed authorization header") {
+		t.Error("Malform data produce non-empty API key or wrong error")
+	}
+
+	t.Error("This is intentional.")
+
+}
